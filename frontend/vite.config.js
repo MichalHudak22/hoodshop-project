550eb1974fca76d605d9598e2316ec7b88f7b10a
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-08-04 14:05:29 +0200

    quick update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
-  server: {
-    proxy: {
-      '/products': import.meta.env.VITE_API_BASE_URL,
-      '/api': import.meta.env.VITE_API_BASE_URL,
-    }
-  },
-});
+export default ({ mode }) => {
+  // načítame env premenné podľa aktuálneho režimu (mode)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    css: {
+      postcss: './postcss.config.js',
+    },
+    server: {
+      proxy: {
+        '/products': env.VITE_API_BASE_URL,
+        '/api': env.VITE_API_BASE_URL,
+      },
+    },
+  });
+};
