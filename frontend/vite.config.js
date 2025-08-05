bfa30d3ee28c378c7a774c9717ff466aaa3c7d52
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-08-05 18:45:31 +0200

    quick update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
-  // načítame env premenné podľa aktuálneho režimu (mode)
   const env = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
-    base: './', // 💥 TOTO PRIDAJ – dôležité pre správne načítanie súborov z dist
+    base: '/', // zmena z './' na '/'
     plugins: [react()],
     css: {
       postcss: './postcss.config.js',
