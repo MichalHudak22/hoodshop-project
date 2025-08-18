076b3eccab8674d416c77b0831fcc9fdc4549b9f
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-08-18 19:23:22 +0200

    quick update

--- frontend/src/admin/UserDetailPage.jsx
+++ frontend/src/admin/UserDetailPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
 // ... všetky importy ostávajú rovnaké
