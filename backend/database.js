f93dbc78b5b67c9843a3a2e50a0967396345c158
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-09-02 09:48:56 +0200

    quick update

--- backend/database.js
+++ backend/database.js
@@ -1,19 +1,24 @@
 const mysql = require('mysql2');
 const util = require('util');
+const fs = require('fs');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,     
-  user: process.env.DB_USER,      
-  password: process.env.DB_PASSWORD,  
-  database: process.env.DB_NAME   
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync('./ssl/isrgrootx1.pem')
+  }
 });
 
 db.connect(err => {
   if (err) {
     console.error('Chyba pripojenia:', err);
   } else {
-    console.log('Pripojený k databáze');
+    console.log('Pripojený k databáze cez SSL');
   }
 });
 
