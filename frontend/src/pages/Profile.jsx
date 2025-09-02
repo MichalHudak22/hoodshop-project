cf9da81186d2e0a270f382f7a9b7aae3337614bf
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-09-02 12:45:21 +0200

    quick update

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -254,10 +254,17 @@ function Profile() {
               {/* Miesto na zobrazenie fotky */}
               <div className="w-56 h-56 rounded-full overflow-hidden border-2 border-gray-400">
                 <img
-                  src={user.user_photo ? `${baseURL}${user.user_photo}` : "/img/default-avatar.jpg"}
+                  src={
+                    user.user_photo
+                      ? user.user_photo.startsWith('http')
+                        ? user.user_photo          // už celá URL, napr. Cloudinary
+                        : `${baseURL}${user.user_photo}` // lokálny obrázok na serveri
+                      : "/img/default-avatar.jpg"       // default avatar
+                  }
                   alt="Profilová fotka"
                   className="w-full h-full object-cover"
                 />
+
               </div>
 
               {/* Tlačidlo Default Photo */}
