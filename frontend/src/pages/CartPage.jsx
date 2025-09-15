bbd26810ced8c356636f19e690f44677ee08d360
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-09-15 16:21:44 +0200

    quick update

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -124,7 +124,7 @@ useEffect(() => {
       <div className="absolute inset-0 bg-black bg-opacity-30 md:bg-opacity-50 z-0" />
 
       {/* Obsah s priehľadným pozadím */}
-      <div className="relative z-10 max-w-4xl mx-auto p-6 bg-black bg-oapcity-30 md:bg-opacity-50 lg:rounded-xl lg:border-2 border-gray-500">
+      <div className="relative z-10 max-w-4xl mx-auto p-6 lg:rounded-xl lg:border-2 border-gray-500">
         <h1 className="text-2xl lg:text-3xl font-bold mb-4 text-center py-3 text-blue-200">
           Shopping Cart
         </h1>
