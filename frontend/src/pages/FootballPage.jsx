8403e7a4a91b46770301612382ecc57c102e1d04
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-10-08 20:57:57 +0200

    quick update

--- frontend/src/pages/FootballPage.jsx
+++ frontend/src/pages/FootballPage.jsx
@@ -110,9 +110,9 @@ const FootballPage = () => {
     <div>
       {/* HEAD TITLE */}
       <section
-        className="relative text-center py-10 px-4 bg-gradient-to-br from-green-600 via-black to-green-700 text-white overflow-hidden border-b-4 border-black bg-opacity-50"
+        className="relative text-center py-10 px-4 bg-gradient-to-br from-green-600 via-black to-green-700 text-white overflow-hidden border-b-4 border-black"
       >
- 
+        <div className="absolute inset-0 bg-[url('/img/football-bg.jpg')] bg-cover bg-center opacity-70"></div>
         <div className="relative z-10 max-w-4xl mx-auto">
           <h1 className="text-xl md:text-3xl lg:text-4xl font-bold mb-4 tracking-wide drop-shadow-md">
             {parseColorTags(titleFromDB)}
