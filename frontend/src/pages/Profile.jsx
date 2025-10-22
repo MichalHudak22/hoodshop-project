14d85bbe7bcb3133b7ccc4d94f085c4ab96c365c
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-10-22 17:20:22 +0200

    quick update

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -257,7 +257,7 @@ function Profile() {
             <h1 className="text-2xl lg:text-3xl font-bold mb-2 text-center text-blue-200">Welcome {user.name}</h1>
             <p className='text-center text-xs pb-5'>Upload a photo to use as your profile avatar.</p>
 
-            <div className="flex flex-col items-center gap-5">
+            <div className="flex flex-col items-center gap-3 lg:gap-5">
 
               {/* Miesto na zobrazenie fotky */}
               <div className="w-56 h-56 rounded-full overflow-hidden border-2 border-gray-400 shadow-xl shadow-black">
