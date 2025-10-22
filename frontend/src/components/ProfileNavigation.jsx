5da7f572e24bf591db9864d0f30407e9132ecb75
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-10-22 17:04:40 +0200

    quick update

--- frontend/src/components/ProfileNavigation.jsx
+++ frontend/src/components/ProfileNavigation.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function ProfileNavigation() {
   return (
 <div className="w-full bg-black lg:mb-4">
-  <div className="flex w-full max-w-5xl mx-auto h-[80px] sm:h-[70px] lg:max-w-full lg:justify-around">
+  <div className="flex w-full max-w-5xl mx-auto h-[60px] sm:h-[70px] lg:max-w-full lg:justify-around">
     <Link
       to="/profile"
       className="flex-1 flex items-center justify-center bg-black text-white hover:text-blue-200 text-[12px] sm:text-[16px] lg:text-[20px] font-bold transition-all duration-300 border border-gray-500 hover:border-blue-200"
