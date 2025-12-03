aaf0034a593e7db0cb06b2cf5f23dd59eb52ecb4
MichalHudak22 <163463321+MichalHudak22@users.noreply.github.com>
2025-12-03 12:20:00 +0100

    quick update

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -27,7 +27,7 @@ const ProjectInfo = () => {
       className="relative min-h-[100vh] text-white flex flex-col items-center bg-fixed bg-cover bg-no-repeat bg-center"
       style={{ backgroundImage: "url('/img/bg-profile-1.jpg')" }}
     >
-      <div className="pt-8 text-center bg-black w-full z-10">
+      <div className="pt-8 text-center bg-black/50 w-full z-10">
         <h1 className="text-2xl lg:text-4xl font-bold text-white">
           About This <span className="text-blue-200">Project</span>
         </h1>
@@ -164,7 +164,7 @@ const ProjectInfo = () => {
           </motion.ul>
 
           <motion.p
-            className="text-white text-center"
+            className="text-white sm:text-center"
             variants={fadeInUp}
             initial="hidden"
             animate="visible"
@@ -175,7 +175,7 @@ const ProjectInfo = () => {
 
 
           <motion.p
-            className="text-white text-center"
+            className="text-white sm:text-center"
             variants={fadeInUp}
             initial="hidden"
             animate="visible"
